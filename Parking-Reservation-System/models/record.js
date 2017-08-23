b071cf76633cc6a64bd814e445dd23dabb844703
ChenLongtian <ChenLongtian@users.noreply.github.com>
2017-08-23 22:58:32 +0800

    添加了record对象和通过用户名查找其预约记录的方法

--- Parking-Reservation-System/models/record.js
+++ Parking-Reservation-System/models/record.js
@@ -14,3 +14,37 @@ pool.on('acquire', function (connection) {
     console.log("Connection %d acquired", connection.threadId);
 
 });
+
+function Record(record){
+    this.reserveid = record.reserveid;
+    this.username = record.username;
+    this.psid= record.psid;
+    this.reservenow=record.reservenow;
+    this.reservedate=record.reservedate;
+    this.reserveintime=record.reserveintime;
+    this.reserveouttime=record.reserveouttime;
+}
+
+//get user 's reserve record by username
+Record.getRecordByUserName = function getRecordByName(username, callback) {
+
+    var getRecordByUserName_Sql = "SELECT * FROM RESERVE_RECORD WHERE USER_NAME = ?";
+
+    pool.getConnection(function (err, connection) {
+
+        if (err) throw err;
+        connection.query(getRecordByUserName_Sql, [username], function (err, result) {
+
+            if (err) {
+                console.log("getRecordByUserName Error: " + err.message);
+                return
+            }
+
+            connection.release();
+            callback(err, result);
+
+        });
+
+    });
+
+};
